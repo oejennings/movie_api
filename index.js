20ded1862271d3d533c5e0220632ad593a80f606
Olivia Jennings <oejennings@users.noreply.github.com>
2023-07-13 12:34:22 -0400

    added JSON object containing data about 10 movies

--- index.js
+++ index.js
@@ -1,2 +1,49 @@
 const express = require('express');
 const app = express();
+
+let topMovies = [
+    {
+        title: 'Clue',
+        director: 'Jonathan Lynn'
+    },
+    {
+        title: 'Avengers',
+        director: 'Joss Whedon' 
+    },
+    {
+        title: 'Iron Man',
+        director: 'Jon Favreau'
+    },
+    {
+        title: 'The Greatest Showman',
+        director: 'Michael Gracey'
+    },
+    {
+        title: '',
+        director: ''
+    },
+    {
+        title: '',
+        director: ''
+    },
+    {
+        title: '',
+        director: ''
+    },
+    {
+        title: '',
+        director: ''
+    },
+    {
+        title: '',
+        director: ''
+    },
+    {
+        title: '',
+        director: ''
+    }
+];
+
+app.get('/movies', (req, res) => {
+    res.json(topMovies);
+});
