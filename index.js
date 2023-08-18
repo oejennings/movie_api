c1d5f61c2243218fa800516494569e11b5eab6e9
Olivia Jennings <oejennings@users.noreply.github.com>
2023-08-18 13:17:25 -0400

    updated connection URI

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const Directors = Models.Director;
 
 // mongoose.connect('mongodb://127.0.0.1/myflixDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
-mongoose.connect('process.env.CONNECTION_URI', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.CONNECTION_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(bodyParser.json());
 
