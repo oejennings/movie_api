3a4d5f04bf05246119679287b191f641c9584bbf
Olivia Jennings <oejennings@users.noreply.github.com>
2023-09-12 11:21:42 -0400

    commented out cors

--- index.js
+++ index.js
@@ -20,21 +20,21 @@ app.use(bodyParser.json());
 
 app.use(morgan('common'));
 
-const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+// const cors = require('cors');
+// let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
 
-const {check, validationResult} = require('express-validator');
+// const {check, validationResult} = require('express-validator');
 
-app.use(cors({
-    origin: (origin, callback) => {
-        if(!origin) return callback(null, true);
-        if(allowedOrigins.indexOf(origin) === -1){ //if a specific origin isn't found on the list of allowed origins
-            let message = 'The CORS policy for this application does not allow access from origin' + origin;
-            return callback(new Error(message ), false);
-        }
-        return callback(null, true);
-    }
-}));
+// app.use(cors({
+//     origin: (origin, callback) => {
+//         if(!origin) return callback(null, true);
+//         if(allowedOrigins.indexOf(origin) === -1){ //if a specific origin isn't found on the list of allowed origins
+//             let message = 'The CORS policy for this application does not allow access from origin' + origin;
+//             return callback(new Error(message ), false);
+//         }
+//         return callback(null, true);
+//     }
+// }));
 
 let auth = require('./auth')(app);
 const passport = require('passport');
